21b4dd5cd0bef7cae2c301509192ef0bd9842f25
Jabez Villan C. Pastores <152822763+JabezPastores@users.noreply.github.com>
2025-03-20 21:43:35 +0800

    Add files via upload

---
+++ Activity1.php
@@ -0,0 +1,165 @@
+<html>
+    <head>
+        <title>Activity 1</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.js"></script>
+    </head>
+    <body>
+        <h1>Activity 1</h1>
+        <div class="container">
+        <table class="table">
+            <tr class="danger">
+                <th>ID</th>
+                <th>Name</th>
+                <th>Remakrs</th>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Hunter</td>
+                <td>Passed</td>
+            </tr>
+            <tr>
+                <td>002</td>
+                <td>Haely</td>
+                <td>Passed</td>
+            </tr>
+            <tr>
+                <td>003</td>
+                <td>Harvey</td>
+                <td>Failed</td>
+            </tr>
+        </table>
+        <table class="table table-striped">
+            <tr class="info">
+                <th>ID</th>
+                <th>Name</th>
+                <th>Remakrs</th>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Hunter</td>
+                <td>Passed</td>
+            </tr>
+            <tr>
+                <td>002</td>
+                <td>Haely</td>
+                <td>Passed</td>
+            </tr>
+            <tr>
+                <td>003</td>
+                <td>Harvey</td>
+                <td>Failed</td>
+            </tr>
+        </table>
+        <table class="table table-condensed">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Remakrs</th>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Hunter</td>
+                <td>Passed</td>
+            </tr>
+            <tr>
+                <td>002</td>
+                <td>Haely</td>
+                <td>Passed</td>
+            </tr>
+            <tr>
+                <td>003</td>
+                <td>Harvey</td>
+                <td>Failed</td>
+            </tr>
+        </table>
+        <table class="table table-hover table-bordered">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Remakrs</th>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Hunter</td>
+                <td>Passed</td>
+            </tr>
+            <tr>
+                <td>002</td>
+                <td>Haely</td>
+                <td>Passed</td>
+            </tr>
+            <tr>
+                <td>003</td>
+                <td>Harvey</td>
+                <td>Failed</td>
+            </tr>
+        </table>
+        <table class="table table-bordered">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Remakrs</th>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Hunter</td>
+                <td>Passed</td>
+            </tr>
+            <tr>
+                <td>002</td>
+                <td>Haely</td>
+                <td>Passed</td>
+            </tr>
+            <tr>
+                <td>003</td>
+                <td>Harvey</td>
+                <td>Failed</td>
+            </tr>
+        </table>
+        </div>
+
+
+        <hr/>
+        <br/>
+        <div class="container">
+            <p class="text-success bg-success">Success</p>
+            <p class="text-danger bg-danger">Danger</p>
+            <p class="text-info bg-info">Info</p>
+            <p class="text-primary bg-primary">Primary</p>
+            <p class="text-muted">Muted</p>
+            <p class="text-warning bg-warning">Warning</p>
+
+            <p class="lead">Computer Science</p>
+            <p class="small">Computer Science</p>
+            <p class="text-center">Computer Science</p>
+            <p class="text-end">Computer Science</p>
+            <p class="text-uppercase">Computer Science</p>
+            <p class="text-lowercase">Computer Science</p>
+        </div>
+
+
+        </hr>
+        </br>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-1" style="background: green;">grid</div>
+                <div class="col-md-1" style="background: red;">grid</div>
+                <div class="col-md-5" style="background: blue;">grid</div>
+            </div>
+            <div class ="row">
+                <div class="col-md-6" style="background: orange;">grid</div>
+            </div>
+
+        <div class="container-fluid">
+            <header class = "container">
+                <h1>Container Fluid</h1>
+            </header>
+        </div>    
+            
+        <div class="jumbtron">
+            <h1>Hello!</h1>
+        </div>
+    </body>
+</html>
